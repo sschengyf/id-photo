16eb1304a4cbede17f94eecbc2c20466532ea921
Ivan Cheng <sschengyf@gmail.com>
2024-04-20 18:21:59 +1200

    feat(app): integrate app with image processor

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,48 @@
-const App = () => (
-  <div>
-    <h1>Hello, World!!!</h1>
-  </div>
-);
+import { useState } from 'react';
+import { process_image } from 'image-processor';
+
+const App = () => {
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+      const fileType = file.type; // image/png
+
+      reader.onload = async (e) => {
+        const arrayBuffer = e.target?.result;
+        if (arrayBuffer && typeof arrayBuffer !== 'string') {
+          const uint8Array = new Uint8Array(arrayBuffer);
+
+          const processedData = process_image(
+            uint8Array,
+            fileType.replace('image/', '')
+          );
+
+          // Convert Vec<u8> to Uint8Array
+          const processedImgUint8Array = new Uint8Array(processedData);
+
+          // Create Blob from Uint8Array
+          const blob = new Blob([processedImgUint8Array], { type: fileType });
+
+          // Create object URL from Blob
+          setImageUrl(URL.createObjectURL(blob));
+        }
+      };
+
+      reader.readAsArrayBuffer(file);
+    }
+  };
+
+  return (
+    <>
+      <h1>Hello, World!!!</h1>
+      <input type="file" accept="image/*" onChange={handleImageUpload} />
+      {imageUrl && <img src={imageUrl} alt="Uploaded Image" />}
+    </>
+  );
+};
 
 export default App;
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -36,4 +36,7 @@ module.exports = {
       },
     ],
   },
+  experiments: {
+    asyncWebAssembly: true,
+  },
 };
