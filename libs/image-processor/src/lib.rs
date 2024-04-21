9dfa7696f715906839f088ea66d340aaaf9a5b40
Ivan Cheng <sschengyf@gmail.com>
2024-04-21 13:54:53 +1200

    feat(image-processor): support jpeg and png

--- libs/image-processor/src/lib.rs
+++ libs/image-processor/src/lib.rs
@@ -1,16 +1,13 @@
 extern crate image;
 
 use wasm_bindgen::prelude::*;
-use image::{GenericImageView, Rgba, ImageBuffer, ImageFormat};
+use image::{DynamicImage, GenericImageView, ImageBuffer, ImageFormat, Rgb, Rgba};
 
 #[wasm_bindgen]
 pub fn process_image(image_data: &[u8], format: &str) -> Vec<u8> {
-
     let image_format = match format.to_lowercase().as_str() {
         "jpeg" | "jpg" => ImageFormat::Jpeg,
         "png" => ImageFormat::Png,
-        "gif" => ImageFormat::Gif,
-        "bmp" => ImageFormat::Bmp,
         _ => {
             // Unsupported format, return an empty Vec<u8> or handle the error accordingly
             return Vec::new();
@@ -26,8 +23,40 @@ pub fn process_image(image_data: &[u8], format: &str) -> Vec<u8> {
     // Resize the image
     let resized_image = img.resize_exact(target_width, target_height, image::imageops::FilterType::Lanczos3);
 
-    let (id_photo_width, id_photo_height) = resized_image.dimensions();
+    if image_format == ImageFormat::Jpeg {
+        process_jpeg(resized_image)
+    } else {
+        process_png(resized_image)
+    }
+}
 
+fn process_jpeg(resized_image: DynamicImage) -> Vec<u8> {
+    let (id_photo_width, id_photo_height) = resized_image.dimensions();
+    let margin = 10; // Adjust spacing as needed
+
+    let collage_width = 3 * id_photo_width;
+    let collage_height = 2 * id_photo_height;
+    let mut collage = ImageBuffer::from_pixel(collage_width, collage_height, Rgb([255, 255, 255]));
+
+    for y in 0..2 {
+        for x in 0..3 {
+            let x_offset = x * (id_photo_width + margin);
+            let y_offset = y * (id_photo_height + margin);
+            image::imageops::overlay(&mut collage, &resized_image.to_rgb8(), x_offset.into(), y_offset.into());
+        }
+    }
+
+     // Convert the resized image to a Vec<u8>
+    let mut output_data = Vec::new();
+    let mut output_cursor = std::io::Cursor::new(&mut output_data);
+    collage.write_to(&mut output_cursor, ImageFormat::Jpeg).unwrap();
+
+    // Return the processed image data
+    output_data
+}
+
+fn process_png(resized_image: DynamicImage) -> Vec<u8> {
+    let (id_photo_width, id_photo_height) = resized_image.dimensions();
     let margin = 10; // Adjust spacing as needed
 
     let collage_width = 3 * id_photo_width;
@@ -43,10 +72,10 @@ pub fn process_image(image_data: &[u8], format: &str) -> Vec<u8> {
     }
 
      // Convert the resized image to a Vec<u8>
-     let mut output_data = Vec::new();
-     let mut output_cursor = std::io::Cursor::new(&mut output_data);
-     collage.write_to(&mut output_cursor, image_format).unwrap();
+    let mut output_data = Vec::new();
+    let mut output_cursor = std::io::Cursor::new(&mut output_data);
+    collage.write_to(&mut output_cursor, ImageFormat::Png).unwrap();
 
-     // Return the processed image data
-     output_data
+    // Return the processed image data
+    output_data
 }
