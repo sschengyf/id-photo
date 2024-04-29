51de0391d2cf2a7b511a302df3c79f27dc26fab6
Ivan Cheng <sschengyf@gmail.com>
2024-04-29 21:12:37 +1200

    feat(app): update page layout

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,14 @@
-import { Home } from './Pages';
+import { Home } from './pages';
 
 const App = () => {
   return (
-    <div className="container mx-auto pt-28 bg-white h-screen flex flex-col">
+    <div className="container mx-auto pt-28 bg-white h-screen flex flex-col max-w-screen-xl">
       <header className="flex flex-row justify-end flex-none">
         <div className="basis-1/2">
-          <h1 className="text-5xl">
+          <h1 className="text-5xl font-bold leading-tight">
             Get your print-ready passport photo in minutes
           </h1>
-          <p className="text-lg text-gray-400">
+          <p className="text-lg text-gray-400 mt-2.5">
             Why spending $20 on passport photo? Simply upload your photo and
             we'll automatically adjust it to the 6x4 inch layout for flawless
             printing.
--- app/src/Pages/Home.tsx
+++ app/src/Pages/Home.tsx
@@ -1,45 +1,127 @@
 import { process_image } from 'image-processor';
 import { useState } from 'react';
+import { FileIcon } from '../components';
+import './Home.css';
 
 export const Home = () => {
   const [imageUrl, setImageUrl] = useState('');
+  const [isDraggingOver, setIsDraggingOver] = useState(false);
+
+  const handleFile = (file: File) => {
+    const reader = new FileReader();
+    const fileType = file.type;
+
+    reader.onload = async (e) => {
+      const arrayBuffer = e.target?.result;
+      if (arrayBuffer && typeof arrayBuffer !== 'string') {
+        const uint8Array = new Uint8Array(arrayBuffer);
+
+        const processedData = process_image(
+          uint8Array,
+          fileType.replace('image/', '')
+        );
+
+        // Convert Vec<u8> to Uint8Array
+        const processedImgUint8Array = new Uint8Array(processedData);
+
+        // Create Blob from Uint8Array
+        const blob = new Blob([processedImgUint8Array], { type: fileType });
+
+        // Create object URL from Blob
+        setImageUrl(URL.createObjectURL(blob));
+      }
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
-
     if (file) {
-      const reader = new FileReader();
-      const fileType = file.type; // image/png
-
-      reader.onload = async (e) => {
-        const arrayBuffer = e.target?.result;
-        if (arrayBuffer && typeof arrayBuffer !== 'string') {
-          const uint8Array = new Uint8Array(arrayBuffer);
-
-          const processedData = process_image(
-            uint8Array,
-            fileType.replace('image/', '')
-          );
-
-          // Convert Vec<u8> to Uint8Array
-          const processedImgUint8Array = new Uint8Array(processedData);
-
-          // Create Blob from Uint8Array
-          const blob = new Blob([processedImgUint8Array], { type: fileType });
-
-          // Create object URL from Blob
-          setImageUrl(URL.createObjectURL(blob));
-        }
-      };
-
-      reader.readAsArrayBuffer(file);
+      handleFile(file);
     }
   };
 
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDraggingOver(false);
+    const file = event.dataTransfer.files?.[0];
+    if (file) {
+      handleFile(file);
+    }
+  };
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDraggingOver(true);
+  };
+
+  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDraggingOver(true);
+  };
+
+  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDraggingOver(false);
+  };
+
   return (
-    <>
-      <input type="file" accept="image/*" onChange={handleImageUpload} />
+    <div className="mt-24">
+      <form>
+        <div className="flex flex-row justify-center">
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Photo size
+            </label>
+            <div>
+              <select className="block appearance-none w-full border border-gray-200 text-gray-500 py-3 px-5 rounded">
+                <option>3 x 4 cm</option>
+                <option>3.5 x 4.5 cm</option>
+                <option>5.1 x 5.1 cm</option>
+              </select>
+            </div>
+          </div>
+          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Print size
+            </label>
+            <div>
+              <select className="block appearance-none w-full border border-gray-200 text-gray-500 py-3 px-5 rounded">
+                <option>10 x 15 cm (4 x 6 inches)</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div
+          className={
+            'm-auto w-2/4 rounded-md border-2 border-dashed py-8 mt-10 text-center cursor-pointer' +
+            (isDraggingOver ? ' border-blue-500' : ' border-gray-300')
+          }
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+        >
+          <div>
+            <FileIcon className="m-auto" />
+          </div>
+          <p className="mt-4">Select a file or drag and drop here</p>
+          <label>
+            <div className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 file-button cursor-pointer inline-block mt-4">
+              <span className="pl-2">Select photo</span>
+            </div>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+              className="hidden"
+            />
+          </label>
+          <p className="mt-4">We don't store your photos</p>
+        </div>
+      </form>
       {imageUrl && <img src={imageUrl} alt="Uploaded Image" />}
-    </>
+    </div>
   );
 };
