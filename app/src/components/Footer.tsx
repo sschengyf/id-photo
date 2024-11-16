ca9e2cd471561daaef2827193da041d63a8c3945
Ivan Cheng <sschengyf@gmail.com>
2024-11-17 10:11:53 +1300

    update keywords

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -4,7 +4,6 @@ export const Footer = () => (
   <footer className="text-center text-gray-400 text-sm flex-none py-6 ">
     <div className="underline flex flex-row justify-center gap-4">
       <a href="mailto:theidphoto@gmail.com">Contact us</a>
-      <a href="https://buymeacoffee.com/theidphoto">Buy me a coffee</a>
       <Link to="/privacy-policy">Privacy policy</Link>
     </div>
     <div className="mt-4">Copyright &copy; 2024 theidphoto.com</div>
