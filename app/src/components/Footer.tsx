5c23fe46d6054ba6c597bc005e0cb163a39a8cac
Ivan Cheng <sschengyf@gmail.com>
2025-01-10 13:21:20 +1300

    fix: update year

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
 
-export const Footer = () => (
-  <footer className="text-center text-gray-400 text-sm flex-none py-6 ">
-    <div className="underline flex flex-row justify-center gap-4">
-      <a href="mailto:theidphoto@gmail.com">Contact us</a>
-      <Link to="/privacy-policy">Privacy policy</Link>
-    </div>
-    <div className="mt-4">Copyright &copy; 2024 theidphoto.com</div>
-  </footer>
-);
+export const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="text-center text-gray-400 text-sm flex-none py-6 ">
+      <div className="underline flex flex-row justify-center gap-4">
+        <a href="mailto:theidphoto@gmail.com">Contact us</a>
+        <Link to="/privacy-policy">Privacy policy</Link>
+      </div>
+      <div className="mt-4">Copyright &copy; {currentYear} theidphoto.com</div>
+    </footer>
+  );
+};
